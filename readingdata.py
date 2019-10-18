14b7401e2ab21a22d94ce6081e7d8c8a4f7dd91c
KingCodeST <cyfotyasi@gmail.com>
2019-10-18 18:56:42 +0200

    commit work

--- readingdata.py
+++ readingdata.py
@@ -2,4 +2,16 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-read = pd.read_csv("tempdata.csv")
+df =pd.read_csv("tempdata.csv")
+
+
+temperature =df['temperature']
+
+datetime =df['date_time']
+
+
+plt.scatter(temperature,datetime, edgecolors ='r')
+plt.xlabel('temperature(Deg.C)')
+plt.ylabel('datetime([YYYYMMDDHHMMSS])')
+plt.title(' Sea Surface Temperature timeseries for an undisclosed location in the ocean.')
+
